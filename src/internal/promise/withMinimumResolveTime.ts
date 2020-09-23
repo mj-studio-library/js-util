21bd74c473cd85489a119fb5920570954da63f9f
MJ Studio <mym0404@gmail.com>
2020-09-23 17:23:29 +0900

    before remove

--- src/internal/promise/withMinimumResolveTime.ts
+++ src/internal/promise/withMinimumResolveTime.ts
@@ -1,16 +1,16 @@
-export default function withMinimumResolveTime<T>(milli: number, promise: Promise<T>): Promise<T> {
+export default function withMinimumResolveTime<T>(minimumMilli: number, promise: Promise<T>): Promise<T> {
   return new Promise((resolve, reject) => {
     const start = Date.now();
     promise
       .then((result) => {
         const diff = Date.now() - start;
 
-        if (diff > milli) {
+        if (diff > minimumMilli) {
           resolve(result);
         } else {
           setTimeout(() => {
             resolve(result);
-          }, milli - diff);
+          }, minimumMilli - diff);
         }
       })
       .catch(reject);
